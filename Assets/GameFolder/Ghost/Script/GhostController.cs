54ff20e52886769e6707082e16c513c930d5848d
Lhsales <henri.luhsales@gmail.com>
2022-10-26 09:13:08 -0300

    Criação do Ghost e organização de inimigos

---
+++ Assets/GameFolder/Ghost/Script/GhostController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    public Transform a;
+    public Transform b;
+    public Transform skin;
+
+    public bool goRight;
+
+    private float velocidade = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Rota
+        if (goRight)
+        {
+            skin.localScale = new Vector3(-1, 1, 1);
+
+            if (Vector2.Distance(transform.position, b.position) < 0.1f)
+            {
+                transform.position = a.position;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, b.position, velocidade * Time.deltaTime);
+        }
+        else
+        {
+            skin.localScale = new Vector3(1, 1, 1);
+
+            if (Vector2.Distance(transform.position, a.position) < 0.1f)
+            {
+                transform.position = b.position;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, a.position, velocidade * Time.deltaTime);
+        }
+        #endregion
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            collision.GetComponent<Character>().life--;
+    }
+}
